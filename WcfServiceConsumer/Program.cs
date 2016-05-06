2eb52417048f6d7444624aa08a9775bc30d2a54f
adarshdch <adarshdch@gmail.com>
2016-05-06 20:49:39 +0530

    Initial Draft

---
+++ WcfServiceConsumer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WcfServiceConsumer
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			try{
+				using(var client = new WindowsAuthWcfServiceRef.Service1Client())
+				{
+					Console.WriteLine("Please enter your name: ");
+					var input = Console.ReadLine();
+					Console.WriteLine(String.Format("Greeting from WCF Service: {0}", client.Greet(input)));
+
+					Console.ReadKey();
+				}
+
+			}
+			catch(Exception e)
+			{
+				Console.WriteLine("Exception occured: " + e.Message);
+				Console.ReadKey();
+			}
+		}
+	}
+}
